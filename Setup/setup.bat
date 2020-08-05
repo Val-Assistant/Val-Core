52eb9e56d5e44ae6216a832729159a824afee3ca
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-08-05 14:39:27 -0300

    Add files via upload

---
+++ Setup/setup.bat
@@ -0,0 +1,10 @@
+  
+pip install speech_recognition
+pip install os
+pip install pyaudio
+pip install gtts
+pip install playsound
+pip install matplotlib
+pip install requests
+pip install bs4
+pip install numpy
