b766da2d7a265401c4d5de43a5eb28aadb2d6174
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-08-07 17:34:13 -0300

    (2.0)

--- Front-End/Javascript/Main.js
+++ Front-End/Javascript/Main.js
@@ -1,12 +1,14 @@
 var capturando = "";
 var msg = ""
+var nova_div = document.createElement("div")
 
 function capturar () {
     var mbox = document.querySelector(".return");
     capturando = document.getElementById('name').value;
     msg = document.createElement("p");
-    msg.textContent = capturando;
-    console.log(msg)
-    mbox.appendChild(msg);
+    msg.textContent = "Você: " + capturando;
+    mbox.appendChild(nova_div);
+    ref = document.querySelector("nav div div");
+    ref.appendChild(msg);
 }
 
