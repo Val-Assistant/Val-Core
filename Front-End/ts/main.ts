499eea2cd9f8c6903011b20e22d8bb16a957f067
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-08-31 18:23:57 -0300

    Add files via upload

---
+++ Front-End/ts/main.ts
@@ -0,0 +1,13 @@
+var capturando = "";
+var nova_div = document.createElement("div")
+
+function capturar () {
+    var mbox = document.querySelector(".return");
+    var capturando = document.getElementById('name')
+    var textCap = capturando.textContent;
+    var msg = document.createElement("p");
+    msg.textContent = "Você: " + textCap;
+    mbox.appendChild(nova_div);
+    var ref = document.querySelector("nav div div");
+    ref.appendChild(msg);
+}
