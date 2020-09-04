5e4b89760f11dce9181eaad9ae66e43f9603d753
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-04 19:59:53 -0300

    Path Contingences

--- JSON & CSV/json_conv.py
+++ JSON & CSV/json_conv.py
@@ -3,7 +3,7 @@ import audio
 
 def converte_json(dicio):
     try:
-        with open('package.json', 'w') as jf:
+        with open('../JSON & CSV/package.json', 'w') as jf:
             json.dump(dicio, jf, indent=4)
             if dicio['str'] != "Que função você gostaria de usar?": audio.cria_audio(dicio['str'])
     except OSError:
@@ -11,7 +11,7 @@ def converte_json(dicio):
 
 def carrega_json(position):
     try:
-        with open('input.json', 'r') as jf:
+        with open('../JSON & CSV/input.json', 'r') as jf:
             oarq = json.load(jf)
             return oarq[position]
     except json.decoder.JSONDecodeError:
@@ -19,7 +19,7 @@ def carrega_json(position):
 
 def inicializa():
     try:
-        with open('input.json', 'w') as inj:
+        with open('../JSON & CSV/input.json', 'w') as inj:
             json.dump({"input" : ""}, inj, indent=4)
     except OSError:
         print("erro no carregamento")
